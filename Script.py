00123df27b9a49de603880e7630ce5bd9b60a1cf
Vysakh8 <104085062+Vysakh8@users.noreply.github.com>
2023-04-30 15:47:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -34,7 +34,7 @@ class script(object):
     
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and ᗩᒍᗩ᙭  will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and KUMBIDI will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
 1. This bot should have admin privillage.
